706ffd8da8918aaf9187b733c4a1fc9bafde6603
SAM <ashishkumarbhi21@gmail.com>
2026-04-04 01:07:12 +0530

    Refactor Google Sheet submission and scroll code

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ var form = document.forms['google-sheet'];
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-    .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
-    .catch(error => console.error('Error!', error.message))
+    .then(response => { alert("Successfully Submitted ✅");
+    form.reset();
+                      })              
+    .catch(error => console.error('Error!', error.message));
 });
 //Google Sheet connect code-END
 
